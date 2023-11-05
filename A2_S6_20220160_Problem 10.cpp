c772729556f12134be819a77d797018e559b6e46
seif eldin shady <147165294+SeifEldinShady004@users.noreply.github.com>
2023-11-05 20:45:16 +0200

    Update and rename problam10.cpp to A2_S6_20220160_Problem 10.cpp

--- A2_S6_20220160_Problem 10.cpp
+++ A2_S6_20220160_Problem 10.cpp
@@ -1,3 +1,10 @@
+// File: A2_S6_20220160_Problem 10.cpp
+// Purpose:this problam do the word are associated with their alternative words
+// Author:seif eldin shady abdelsalam
+// Section:S6
+// ID:20220160
+// TA:maya ahmed 
+// Date: 5 Nov 2023
 #include <iostream>
 #include <fstream>
 #include <string>
